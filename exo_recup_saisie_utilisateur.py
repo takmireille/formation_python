c29e8ef66ef38e7f9d1f9ddad79f45b99d6ab759
takmireille <nzonteumireille@gmail.com>
2022-04-23 18:21:27 -0700

    ajout exo de base python 2

---
+++ exo_recup_saisie_utilisateur.py
@@ -0,0 +1,21 @@
+# le but est de demander à l'utilisateur d'entrer quelque informations
+
+nom_prenom = input("entrez vos noms et prenoms : ")
+
+print(nom_prenom)
+
+age = input("quel age avez-vous? :")
+
+
+print(age)
+
+
+ville = input("quelle est votre ville de residence")
+
+
+print (ville)
+
+
+
+
+
