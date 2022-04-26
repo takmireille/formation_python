f4eff4811d1154fa373da00b358647bcff04d18e
takmireille <nzonteumireille@gmail.com>
2022-04-25 17:20:16 -0700

    test the if condition

---
+++ structure_cond_simple.py
@@ -0,0 +1,18 @@
+# ecrire une structure conditionnelle qui veridfie la valeur de la variable age
+
+age = input("veuillez entrer  votre age :")
+
+if int(age) >= 18:
+
+	print ("vous êtes majeur!")
+
+if int(age) < 18:
+	
+	print ("vous êtes mineur!")
+
+
+
+	
+
+
+
