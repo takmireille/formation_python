ca3ca1a57c4df36a7d2ab850c06258c09c2c1416
takmireille <nzonteumireille@gmail.com>
2022-04-26 16:15:07 -0700

    manipulation of elements in a list

---
+++ listes.py
@@ -0,0 +1,44 @@
+# manipulation des listes
+
+print("1-recuperer le premier et le dernier chiffre de la liste:")
+
+nombres = [1, 2, 3, 4, 5, 4, 3, 2, 1]
+print (nombres)
+print("le premier nombre de la liste est :")
+nombre_premier = nombres[0]
+
+print (nombre_premier)
+print ("le dernier nombre de la liste est:")
+
+nombre_dernier = nombres[-1]
+
+print (nombre_dernier)
+
+
+print ("2-recuperer l'element python dans la liste de variable language")
+
+languages = ["java", "Python", "C++"]
+
+print (languages)
+
+language = languages[1]
+
+print (language)
+
+print("3- changer la position de l'element puthon dans la liste pour le metre à la fin de la liste")
+
+
+liste = ["java", "Python", "C++"]
+ 
+print (liste)
+liste.remove("Python")
+
+liste.append("Python")
+
+print(liste)
+
+
+
+
+
+
