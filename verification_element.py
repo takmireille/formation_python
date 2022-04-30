8dfeaa359ead58e51f8873345dcf2e586c46a91a
takmireille <nzonteumireille@gmail.com>
2022-04-30 16:16:42 -0700

    adding and checking items in a list

---
+++ verification_element.py
@@ -0,0 +1,19 @@
+# verifier qu'un élément est dans une liste: 
+
+liste = [1, 2, 3, 4, 5]
+
+print (liste)
+
+print ("1-rajouter le nombre 6 dans la liste")
+
+liste.append(6)
+
+
+print (liste)
+
+print("2-vérifier que l'élement a été rajouter")
+
+if 6 in liste:
+	print ("le nombre 6 a été ajouté à la liste")
+
+
